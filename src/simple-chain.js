be1e13a1dd045d1da28a11f5c5e4ca6d4de06b4d
Eugene9533 <79876217+Eugene9533@users.noreply.github.com>
2022-05-02 10:24:04 +0300

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,26 +5,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    chain: [],
+    getLength() {
+        return this.chain.length;
+    },
+    addLink(value) {
+        this.chain.push(`( ${value} )`);
+        return this;
+    },
+    removeLink(position) {
+        if (position <= 0 || !Number.isInteger(position) || position > this.chain.length - 1) {
+            err = new Error("You can't remove incorrect link!");
+            return this;
+        }
+        this.chain.splice(position - 1, 1);
+        return this;
+    },
+    reverseChain() {
+        this.chain.reverse();
+        return this;
+    },
+    finishChain() {
+        if (typeof err !== 'undefined') return err
+        return this.chain.join("~~");
+    }
 };
 
 module.exports = {
